8ec0e18ae3a64ce1a413795f100b2a659ae43d44
leomarzeuski <102033896+leomarzeuski@users.noreply.github.com>
2022-09-02 13:55:48 -0300

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post'
-
+import amouo from './images/amouo.jpeg'
 import './Global.css'
 
 import styles from './App.module.css'
@@ -9,8 +9,52 @@ import { sidebar as Sidebar } from './components/Sidebar'
 const posts = [
   {
     id: 1,
-    author: {},
-    content: []
+    author: {
+      avatarImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPpnraGJEUxCDNtllzvmxOLOPH52Qdb2_CmA&usqp=CAU',
+      name: 'Camel case',
+      role: 'Desenvolvedora Pleno'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content:
+          'Meu objetivo-mor com este livro não é tornar-me bilionário. Aliás, há apenas uma pessoa no mundo que se tornou bilionária tendo como principal atividade profissional escrever livros: J.K. Rowling, autora do fenômeno mundial Harry Potter.'
+      },
+
+      {
+        type: 'paragraph',
+        content:
+          'Minha principal motivação para escrever este livro é a mesma que me fez aceitar fazer parte da “equipe da riqueza” (Wealth Team) da Forbes, um seleto grupo de repórteres da revista norte-americana que cobre a seção de bilionários. Não me formei em jornalismo, caí de paraquedas na Forbes e fiquei profundamente empolgado com o privilégi de trabalhar descobrindo e revelando como os mais bem-sucedidos empresários da atualidade fazem para alcançar – e se manter, né Eike Batista? – o topo e acumular vastas fortunas no caminho.'
+      },
+
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-08-12 08:08:08')
+  },
+  {
+    id: 2,
+    author: {
+      avatarImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh6jAtA0TCTmSo-AhfFdVod7BZdAdZ6L5eAw&usqp=CAU',
+      name: 'Leo Marzeuski',
+      role: 'Desenvolvedor Pleno'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content:
+          'Meu objetivo-mor com este livro não é tornar-me bilionário. Aliás, há apenas uma pessoa no mundo que se tornou bilionária tendo como principal atividade profissional escrever livros: J.K. Rowling, autora do fenômeno mundial Harry Potter.'
+      },
+
+      {
+        type: 'paragraph',
+        content:
+          'Minha principal motivação para escrever este livro é a mesma que me fez aceitar fazer parte da “equipe da riqueza” (Wealth Team) da Forbes, um seleto grupo de repórteres da revista norte-americana que cobre a seção de bilionários. Não me formei em jornalismo, caí de paraquedas na Forbes e fiquei profundamente empolgado com o privilégi de trabalhar descobrindo e revelando como os mais bem-sucedidos empresários da atualidade fazem para alcançar – e se manter, né Eike Batista? – o topo e acumular vastas fortunas no caminho.'
+      },
+
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ]
   }
 ]
 export function App() {
@@ -21,19 +65,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Leo Marzeuski"
-            content=" sdhghh h hdoahdododoah oh hhdaohhdoa hoh ohodh aohdohadhodahohdoa"
-          />
-
-          <Post
-            author="Leo Marzeuski"
-            content=" sdhghh h hdoahdododoah oh hhdaohhdoa hoh ohodh aohdohadhodahohdoa"
-          />
-          <Post
-            author="Leo Marzeuski"
-            content=" sdhghh h hdoahdododoah oh hhdaohhdoa hoh ohodh aohdohadhodahohdoa"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,51 +1,30 @@
-import { ComputerTower } from 'phosphor-react'
+import { format } from 'date-fns'; 21.5k (gzipped: 6k)
 import styles from './Post.module.css'
-import amouo from '../images/amouo.jpeg'
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
-export function Post() {
+
+export function Post({ author, publishedAt }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'"
+  )
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={amouo} />
+          <Avatar src={author.avatarImg} />
           <div className={styles.authorInfo}>
-            <strong> Camila Mendes </strong>
-            <span> Web Developer master senior </span>
+            <strong> {author.name} </strong>
+            <span> {author.role} </span>
           </div>
         </div>
 
-        <time title="12 de Agosto" dateTime="2022-08-12 08:08:08">
-          Publicado a 1h
+        <time tittle="" dateTime="2022-05-11 08:13:30">
+          {publishedDateFormatted}
         </time>
       </header>
-      <div className={styles.content}>
-        <p>
-          Meu objetivo-mor com este livro não é tornar-me bilionário. Aliás, há
-          apenas uma pessoa no mundo que se tornou bilionária tendo como
-          principal atividade profissional escrever livros: J.K. Rowling, autora
-          do fenômeno mundial Harry Potter.
-        </p>
-
-        <p>
-          Minha principal motivação para escrever este livro é a mesma que me
-          fez aceitar fazer parte da “equipe da riqueza” (Wealth Team) da
-          Forbes, um seleto grupo de repórteres da revista norte-americana que
-          cobre a seção de bilionários. Não me formei em jornalismo, caí de
-          paraquedas na Forbes e fiquei profundamente empolgado com o privilégio
-          de trabalhar descobrindo e revelando como os mais bem-sucedidos
-          empresários da atualidade fazem para alcançar – e se manter, né Eike
-          Batista? – o topo e acumular vastas fortunas no caminho.
-        </p>
-
-        <p>
-          {' '}
-          <a href="">
-            <ComputerTower size={20} /> #Progamação
-          </a>{' '}
-          <a href="">#Tecnologia</a>{' '}
-        </p>
-      </div>
+      <div className={styles.content}></div>
       <form className={styles.commentForm}>
         <strong> Deixe seu feedback </strong>
         <textarea placeholder="Deixe um comentario" />
